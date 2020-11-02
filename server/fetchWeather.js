b232fb9b9330e9ae2840be95f5b2027949338f99
Rahul Kadukar <rahulkadukar@yahoo.com>
2020-11-01 23:24:54 -0500

    Processing done in chronological order

--- server/fetchWeather.js
+++ server/fetchWeather.js
@@ -4,6 +4,7 @@ const got = require('got')
 const darkSkyToken = process.env['TOKEN_DARKSKY']
 const { sqlQuery } = require('../utils/postgres')
 
+/* Utility functions */
 Date.prototype.addDays = function(days) {
   const d = new Date(this.valueOf())
   d.setDate(d.getDate() + days)
@@ -13,73 +14,99 @@ Date.prototype.addDays = function(days) {
 async function sleepForMs(ms) { return new Promise(r => setTimeout(r, ms)) }
 function dnow() { return new Date().toISOString() }
 
-function shuffleObject(obj) {
-  const newObj = {}
-  const keys = Object.keys(obj)
+/* Uncomment the line below for testing */
+// init().then(() => { process.exit() })
 
-  keys.sort((a,b) => { return (Math.random() - 0.5) })
-  keys.forEach((k) => { newObj[k] = obj[k]  })
-  return newObj
-}
-
+/*  Main function */
 async function init() {
   const cityList = JSON.parse(fs.readFileSync('./config/cityList.json', 'utf-8'))
-  const shuffledCities = shuffleObject(cityList)
-  await processCities(shuffledCities)
+  await prepareDB(cityList)
+  await processCities(cityList)
+}
+
+async function insertDateInfo(sqlText) {
+  let queryText = `INSERT INTO "public".darksky_dailyweather` +
+    `(cityname, eventtime, rawdata)	VALUES ${sqlText.slice(0, -1)} ` +
+    `ON CONFLICT DO NOTHING`
+  await sqlQuery(queryText)
 }
 
 async function processCities(cityList) {
-  const dateRange = ['2020-01-01', '2020-10-31']
+  const dateArray = []
+  const maxCount = 4
   const url = 'https://api.darksky.net/forecast'
-  const maxCount = 10
+
+  const data = await sqlQuery({
+    text: `SELECT * FROM "public".darksky_dailyweather WHERE rawdata = $1 ` +
+        `ORDER BY eventtime DESC LIMIT ${Math.floor(maxCount * 2)}`,
+    values: [ '{}']
+  })
+
+  data.dbResult.forEach((d) => {
+    const dt = {
+      city: d.cityname,
+      date: new Date(`${d.eventtime.slice(0,10)}T06:00:00.000Z`)
+    }
+    dateArray.push(dt)
+  })
 
   try {
     let c = 0
-    for (const [k, v] of Object.entries(cityList)) {
-      if (c > maxCount) break
-      const lat = v.lat
-      const lon = v.lon
-      const dateArray = []
+    for (let i = 0; i < dateArray.length; ++i) {
+      const city = dateArray[i].city
+      const lat = cityList[city].lat
+      const lon = cityList[city].lon
 
-      const data = await sqlQuery({
-        text: `SELECT eventtime FROM "public".darksky_dailyweather WHERE cityname = $1`,
-        values: [ k ]
-      })
+      const dateToProcess = dateArray[i].date.getTime() / 1000
+      const dataResp = await got(`${url}/${darkSkyToken}/${lat},${lon},${dateToProcess}`)
+      const weatherData = JSON.parse(dataResp.body)
+      const insertText = `UPDATE "public".darksky_dailyweather SET rawdata = '${JSON.stringify(weatherData)}' ` +
+        `WHERE cityname = '${city}' AND eventtime = '${new Date(dateToProcess * 1000).toISOString()}'`
 
-      const dateProcessed = {}
-      data.dbResult.forEach((d) => { dateProcessed[d.eventtime] = 'X' })
+      await sqlQuery(insertText)
+      await sleepForMs(1000)
 
-      let firstDate = new Date(`${dateRange[0]}T06:00:00`)
-      const lastDate = new Date(`${dateRange[1]}T06:00:00`)
-      while (firstDate <= lastDate) {
-        if (dateProcessed[firstDate.toISOString()] !== 'X') dateArray.push(firstDate)
-        firstDate = firstDate.addDays(1)
-      }
-
-      for (let i = 0; i < dateArray.length; ++i) {
-        const dateToProcess = dateArray[i].getTime() / 1000
-        const dataResp = await got(`${url}/${darkSkyToken}/${lat},${lon},${dateToProcess}`)
-        const weatherData = JSON.parse(dataResp.body)
-        const insertData = {
-          text: `INSERT INTO "public".darksky_dailyweather` +
-              `(cityname, eventtime, rawdata)	VALUES ($1, $2, $3)`,
-          values: [k, new Date(dateToProcess * 1000), JSON.stringify(weatherData)]
-        }
-        await sqlQuery(insertData)
-        await sleepForMs(1000)
-
-        console.log(`[${dnow()}]: Processed [${k}] for date ${new Date(dateToProcess * 1000)}`)
-        if (++c > maxCount) break
-      }
+      console.log(`[${dnow()}]: Processed [${city}] for date ${new Date(dateToProcess * 1000)}`)
+      if (++c >= maxCount) break
     }
   } catch (excp) {
     console.log(`[EXCEPTION]: ${excp.message}`)
   } finally {
     // Do nothing
+    console.log(`[${dnow()}]: Finished processing cities`)
+  }
+}
+
+async function prepareDB(cityList) {
+  const dateRange = ['2019-01-01', '2020-10-31']
+  const dateArray = []
+
+  let firstDate = new Date(`${dateRange[0]}T06:00:00.000Z`)
+  const lastDate = new Date(`${dateRange[1]}T06:00:00.000Z`)
+  while (firstDate <= lastDate) {
+    const dateToInsert = new Date(`${firstDate.toISOString().slice(0,10)}T06:00:00.000Z`)
+    dateArray.push(dateToInsert)
+    firstDate = firstDate.addDays(1)
+  }
+
+  let ctr = 0
+  for (const [k, v] of Object.entries(cityList)) {
+    let insertText = ''
+    for (let i = 0; i < dateArray.length; ++i) {
+      const dateToProcess = dateArray[i].getTime() / 1000
+      insertText += `('${k}', '${new Date(dateToProcess * 1000).toISOString()}', '{}'),`
+      if (++ctr % 1000 === 0) {
+        await insertDateInfo(insertText)
+        insertText = ''
+        ctr = 0
+      }
+    }
+
+    if (insertText !== '') { await insertDateInfo(insertText) }
   }
 }
 
 cron.schedule('0 */15 * * * *', () => {
-  console.log(`[${dnow()}]: Starting DarkSky Fetch`);
+  console.log(`[${dnow()}]: Starting DarkSky Fetch`)
   init().then(() => { console.log(`[${dnow()}]: DONE`); return {} })
-});
+})
